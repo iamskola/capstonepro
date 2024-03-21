0f46b259e4e1009d6d60d719e2dc533d2b75a4a0
iamskola <ailerukola@gmail.com>
2024-03-21 11:01:39 +0100

    feat:endpoints

--- Store_App/urls.py
+++ Store_App/urls.py
@@ -7,11 +7,11 @@ urlpatterns=[
     path('categories',views.CategoryEndpoint.as_view(), name='categories'),
     path('category/<int:pk>', views.SingleCategoryEndpoint.as_view(), name='category-details'),
     path('category/<int:pk>/delete',views.CategoryDeleteEndpoint.as_view(), name='delete-category'),
-    #path('products-list', views.ProductEndpoint.as_view(), name='products-list'),
-    path('products', views.ProductListEndpoint.as_view(), name='products'),
+    path('products_create', views.ProductEndpoint.as_view(), name='products_create'),
+    path('products_list_only', views.ProductListEndpoint.as_view(), name='products_list'),
     path('product/<int:product_id>', views.ProductDetailEndpoint.as_view(), name='product-detail'),
-    path('store/', views.StoreListEndpoint.as_view(), name='store'),
-    #path('store-list/', views.StoreEndpoint.as_view(), name='store'),
+    path('store_list_only/', views.StoreListEndpoint.as_view(), name='store_list'),
+    path('store_create/', views.StoreEndpoint.as_view(), name='store'),
     path('store/<int:store_id>', views.StoreDetailEndpoint.as_view(), name='store-detail')
 
 ]
